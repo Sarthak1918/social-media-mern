28486047e278e130580e3616cf4aa201c1ec701d
Sarthak Panda <pandasarthak12345@gmail.com>
2024-03-18 17:53:12 +0530

    mongodb connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,11 @@
 import "dotenv/config.js"
-import express from "express"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-const app = express();
-
-app.get("/",(req,res)=>{
-    res.send("Home")
-})
-
-
-app.listen(process.env.PORT || 6000,()=>{
-    console.log("Server  is running on port 5000");
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 6000,()=>{
+        console.log("SERVER IS RUNNING AT PORT :",process.env.PORT);
+    })
+}).catch((error)=>{
+    console.log("MONGO DB CONNECTION FAILED!!",error);
 })
